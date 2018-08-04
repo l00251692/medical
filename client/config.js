597c71f2d3b00a8ea2fa4c9983a7ee7d9e25e31b
l00251692 <982679298@qq.com>
2018-08-04 12:46:16 +0800

    【功能】补提交返回用户的状态订单管理页面

--- client/config.js
+++ client/config.js
@@ -1,7 +1,7 @@
 
 //var host = "www.ailogic.xin"
 //var host = "127.0.0.1"
-var host = "v7ax8u.natappfree.cc"
+var host = "7n2vqb.natappfree.cc"
 module.exports = {
   host,
   qqmapKey: 'FPOBZ-UT2K2-ZFYUC-CX67E-IOOYS-7XFQ6'
